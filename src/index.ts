c3bfc54269106a6eedb045ce29dd1614fa9c6208
Igor Motyka <igor_motyka@mail.com>
2020-04-21 12:22:33 +0200

    Added error handler and image to product

--- src/index.ts
+++ src/index.ts
@@ -2,21 +2,21 @@ import express from "express";
 import categoriesEndpoints from "./routes/categories";
 import productEndpoints from "./routes/product";
 import activeService from "./services/activeServices";
-
+import cors from "cors";
 const app = express();
 const port = 8080; // default port to listen
+
+app.use(cors())
 
 app.use('/categories',categoriesEndpoints);
 app.use('/products',productEndpoints);
 
-
-//Error handler
-app.use(function (err, req, res, next) {
+// Error handler
+app.use((err, req, res, next) => {
     res.status(500).send(err.message)
 })
 
 app.listen( port, async () => {
     const dbConn = activeService.getDbConnectionService();
     await dbConn.connect();
-    console.log( `server started at http://localhost:${ port }` );
 } );
